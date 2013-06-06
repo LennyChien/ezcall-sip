9068a0ba5f2f6b9e7b44f4ea939d804b1634728c
lenny <lenny.chien@gmail.com>
2013-06-06 11:13:10 +0800

    change Codec priority

--- EZCall/src/tw/net/ezcall/wizards/impl/EZCall.java
+++ EZCall/src/tw/net/ezcall/wizards/impl/EZCall.java
@@ -28,96 +28,96 @@ import tw.net.ezcall.api.SipProfile;
 import tw.net.ezcall.utils.PreferencesWrapper;
 
 public class EZCall extends SimpleImplementation {
-	
 
 	@Override
 	protected String getDomain() {
 		return "voip2.ttinet.com.tw";
 	}
-	
+
 	@Override
 	protected String getDefaultName() {
 		return "EZCall";
 	}
 
-	//Customization
+	// Customization
 	@Override
 	public void fillLayout(final SipProfile account) {
 		super.fillLayout(account);
-		
+
 		accountUsername.getEditText().setInputType(InputType.TYPE_CLASS_PHONE);
-		
+
 	}
-	
+
 	@Override
 	public SipProfile buildAccount(SipProfile account) {
-	    SipProfile acc = super.buildAccount(account);
-	    //acc.reg_timeout = 900;
-	    return acc;
+		SipProfile acc = super.buildAccount(account);
+		// acc.reg_timeout = 900;
+		return acc;
 	}
-	
+
 	@Override
-    public void setDefaultParams(PreferencesWrapper prefs) {
-        super.setDefaultParams(prefs);
-        // Stun
-        //prefs.setPreferenceBooleanValue(SipConfigManager.ENABLE_STUN, true);
-        //prefs.addStunServer("stun01.1worldtelecom.mobi");
-        //prefs.addStunServer("stun02.1worldtelecom.mobi");
-        
-        // User agent -- useful?
-        //prefs.setPreferenceStringValue(SipConfigManager.USER_AGENT, "1WorldVoip");
+	public void setDefaultParams(PreferencesWrapper prefs) {
+		super.setDefaultParams(prefs);
+		// Stun
+		// prefs.setPreferenceBooleanValue(SipConfigManager.ENABLE_STUN, true);
+		// prefs.addStunServer("stun01.1worldtelecom.mobi");
+		// prefs.addStunServer("stun02.1worldtelecom.mobi");
 
-        // Codecs -- Assume they have legal rights to provide g729 to each users
-        // As they activate it by default in their forked app.
-        // For Narrowband
-        prefs.setCodecPriority("PCMU/8000/1", SipConfigManager.CODEC_NB, "100");
-        prefs.setCodecPriority("PCMA/8000/1", SipConfigManager.CODEC_NB, "150");
-        prefs.setCodecPriority("speex/8000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("speex/16000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("speex/32000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("GSM/8000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("G722/16000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("G729/8000/1", SipConfigManager.CODEC_NB, "200");
-        prefs.setCodecPriority("iLBC/8000/1", SipConfigManager.CODEC_NB, "250"); /*
-                                                                                * Disable
-                                                                                * by
-                                                                                * default
-                                                                                */
-        prefs.setCodecPriority("SILK/8000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("SILK/12000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("SILK/16000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("SILK/24000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("CODEC2/8000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("G7221/16000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("G7221/32000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("ISAC/16000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("ISAC/32000/1", SipConfigManager.CODEC_NB, "0");
-        prefs.setCodecPriority("AMR/8000/1", SipConfigManager.CODEC_NB, "0");
+		// User agent -- useful?
+		// prefs.setPreferenceStringValue(SipConfigManager.USER_AGENT,
+		// "1WorldVoip");
 
-        // For Wideband
-        prefs.setCodecPriority("PCMU/8000/1", SipConfigManager.CODEC_WB, "100");
-        prefs.setCodecPriority("PCMA/8000/1", SipConfigManager.CODEC_WB, "150");
-        prefs.setCodecPriority("speex/8000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("speex/16000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("speex/32000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("GSM/8000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("G722/16000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("G729/8000/1", SipConfigManager.CODEC_WB, "200");
-        prefs.setCodecPriority("iLBC/8000/1", SipConfigManager.CODEC_WB, "250"); /*
-                                                                                * Disable
-                                                                                * by
-                                                                                * default
-                                                                                */
-        prefs.setCodecPriority("SILK/8000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("SILK/12000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("SILK/16000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("SILK/24000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("CODEC2/8000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("G7221/16000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("G7221/32000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("ISAC/16000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("ISAC/32000/1", SipConfigManager.CODEC_WB, "0");
-        prefs.setCodecPriority("AMR/8000/1", SipConfigManager.CODEC_WB, "0");
-    }
-	
+		// Codecs -- Assume they have legal rights to provide g729 to each users
+		// As they activate it by default in their forked app.
+		// For Narrowband
+		prefs.setCodecPriority("G729/8000/1", SipConfigManager.CODEC_NB, "250");
+		prefs.setCodecPriority("iLBC/8000/1", SipConfigManager.CODEC_NB, "200");
+		prefs.setCodecPriority("PCMU/8000/1", SipConfigManager.CODEC_NB, "100");
+		prefs.setCodecPriority("PCMA/8000/1", SipConfigManager.CODEC_NB, "150");
+		prefs.setCodecPriority("speex/8000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("speex/16000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("speex/32000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("GSM/8000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("G722/16000/1", SipConfigManager.CODEC_NB, "0");
+
+		/*
+		 * Disable by default
+		 */
+		prefs.setCodecPriority("SILK/8000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("SILK/12000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("SILK/16000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("SILK/24000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("CODEC2/8000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("G7221/16000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("G7221/32000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("ISAC/16000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("ISAC/32000/1", SipConfigManager.CODEC_NB, "0");
+		prefs.setCodecPriority("AMR/8000/1", SipConfigManager.CODEC_NB, "0");
+
+		// For Wideband
+		prefs.setCodecPriority("G729/8000/1", SipConfigManager.CODEC_WB, "250");
+		prefs.setCodecPriority("iLBC/8000/1", SipConfigManager.CODEC_WB, "200");
+		prefs.setCodecPriority("PCMU/8000/1", SipConfigManager.CODEC_WB, "100");
+		prefs.setCodecPriority("PCMA/8000/1", SipConfigManager.CODEC_WB, "150");
+		prefs.setCodecPriority("speex/8000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("speex/16000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("speex/32000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("GSM/8000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("G722/16000/1", SipConfigManager.CODEC_WB, "0");
+		
+		/*
+		 * Disable by default
+		 */
+		prefs.setCodecPriority("SILK/8000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("SILK/12000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("SILK/16000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("SILK/24000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("CODEC2/8000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("G7221/16000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("G7221/32000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("ISAC/16000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("ISAC/32000/1", SipConfigManager.CODEC_WB, "0");
+		prefs.setCodecPriority("AMR/8000/1", SipConfigManager.CODEC_WB, "0");
+	}
+
 }
